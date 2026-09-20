bb978cebaee87092fe4d91a322b721e95d3ab26b
JuanPabloBoj <jboj-2025117@kinal.edu.gt>
2026-09-19 21:03:52 -0600

    CURD's de Postulacin y Historial realizado, base de routes realizada y estilo base de css agregado

--- frontend/src/app/core/models/auth.model.ts
+++ frontend/src/app/core/models/auth.model.ts
@@ -29,7 +29,7 @@ export interface RegisterRequest {
     usuario_correo: string;
     usuario_password: string;
     usuario_rol: RolUsuario;
-    usuario_telefono?: string;
+     usuario_telefono?: string;
     usuario_dpi?: string;
     usuario_profesion?: string;
 }

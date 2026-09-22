1111810bf3d47e5b4b2efefdd24cd5e486ef0542
JuanPabloBoj <jboj-2025117@kinal.edu.gt>
2026-09-21 18:51:50 -0600

    visualizaciones de evaluaciones y perfil del candidato

---
+++ frontend/src/app/pages/admin-dashboard/components/admin-evaluations-list/admin-evaluations-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-admin-evaluations-list',
+  styleUrl: './admin-evaluations-list.css',
+  templateUrl: './admin-evaluations-list.html',
+})
+export class AdminEvaluationsList {}

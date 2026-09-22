fb5ab759dfa86e6cae749e4154f441eedf45502e
aaquino-2025076 <aaquino-2025076@kinal.edu.gt>
2026-09-22 00:41:26 -0600

    primer intento de los component

--- frontend/src/app/shared/components/data-table/data-table.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-data-table',
-  styleUrl: './data-table.css',
-  templateUrl: './data-table.html',
-})
-export class DataTable {}

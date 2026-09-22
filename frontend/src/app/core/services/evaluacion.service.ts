2324eabf446321453a937e3531e2bd3db6e1e119
aaquino-2025076 <aaquino-2025076@kinal.edu.gt>
2026-09-21 23:45:38 -0600

    creacion del service de Evaluacion

--- frontend/src/app/core/services/evaluacion.service.ts
+++ frontend/src/app/core/services/evaluacion.service.ts
@@ -1,4 +1,48 @@
-import { Service } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Evaluacion, EvaluacionResponse } from '../models/evaluacion.model';
 
-@Service()
-export class EvaluacionService {}
+@Injectable({
+  providedIn: 'root'
+})
+export class EvaluacionService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'http://localhost:3000/api/evaluaciones';
+
+  getEvaluaciones(): Observable<Evaluacion[]> {
+    return this.http.get<EvaluacionResponse>(this.apiUrl).pipe(
+      map(res => res.evaluaciones || [])
+    );
+  }
+
+  getEvaluacionById(id: number): Observable<Evaluacion> {
+    return this.http.get<EvaluacionResponse>(`${this.apiUrl}/${id}`).pipe(
+      map(res => {
+        if (!res.evaluacion) {
+          throw new Error('Evaluación no encontrada');
+        }
+        return res.evaluacion;
+      })
+    );
+  }
+
+  createEvaluacion(evaluacion: Evaluacion): Observable<Evaluacion> {
+    return this.http.post<EvaluacionResponse>(this.apiUrl, evaluacion).pipe(
+      map(res => res.evaluacion!)
+    );
+  }
+
+  updateEvaluacion(id: number, evaluacion: Evaluacion): Observable<Evaluacion> {
+    return this.http.put<EvaluacionResponse>(`${this.apiUrl}/${id}`, evaluacion).pipe(
+      map(res => res.evaluacion!)
+    );
+  }
+
+  deleteEvaluacion(id: number): Observable<void> {
+    return this.http.delete<{ message: string }>(`${this.apiUrl}/${id}`).pipe(
+      map(() => void 0)
+    );
+  }
+}
